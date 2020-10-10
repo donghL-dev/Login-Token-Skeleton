7cd9e9d26db6b06448522d485649a3e1f25dd2f8
Dustin Lim <dustinlim@tradipod.com>
2020-10-10 19:53:08 +0900

    create project files

---
+++ src/main/java/com/donghun/logintoken/LoginTokenApplication.java
@@ -0,0 +1,13 @@
+package com.donghun.logintoken;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LoginTokenApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LoginTokenApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/donghun/logintoken/LoginTokenApplicationTests.java
@@ -0,0 +1,14 @@
+package com.donghun.logintoken;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LoginTokenApplicationTests {
+
+    @Test
+    void contextLoads() {
+        LoginTokenApplication.main(new String[]{});
+    }
+
+}
